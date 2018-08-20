8d6d7b4d394373e15b9c2fda044a3fbf84465b4c
Guilherme Corrêa <Guilherme Corra@DESKTOP-IDNFGM4>
2018-08-19 23:02:19 -0300

    Code Funcionario

--- br.senac.dd.exercicio05/src/br/senac/dd/exercicio05/model/vo/FuncionarioVO.java
+++ br.senac.dd.exercicio05/src/br/senac/dd/exercicio05/model/vo/FuncionarioVO.java
@@ -2,8 +2,44 @@ package br.senac.dd.exercicio05.model.vo;
 
 public class FuncionarioVO {
 
+	private int idFuncionario;
 	private String nome;
-	private String cargo;
 	private double salario;
+	private String cpf;
 	
+	public FuncionarioVO() {
+		
+	}
+	
+	public FuncionarioVO(String nome, double salario, String cpf, int idFuncionario) {
+		super();
+		this.nome = nome;
+		this.salario = salario;
+		this.cpf = cpf;
+		this.idFuncionario = idFuncionario;
+	}
+	public int getIdFuncionario() {
+		return idFuncionario;
+	}
+	public void setIdFuncionario(int idFuncionario) {
+		this.idFuncionario = idFuncionario;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
 }
